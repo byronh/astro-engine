02b9c435f433a8aff73758d9dcfc36bf72ca0166
Byron Henze <byronh@gmail.com>
2015-02-22 00:42:03 -0800

    Clean up setup.py

--- astro/window.pyx
+++ astro/window.pyx
@@ -1,12 +1,12 @@
-cimport cwindow
+cimport c_window
 
 
 cdef class Window:
     cdef object config
-    cdef cwindow.Window* window
+    cdef c_window.Window* window
 
     def __dealloc__(self):
-        cwindow.destroy(self.window)
+        c_window.destroy(self.window)
 
     def __init__(self, config):
         self.config = config
@@ -14,26 +14,26 @@ cdef class Window:
 
     def create(self):
         cdef bytes c_title = self.config.title.encode()
-        cwindow.initialize()
+        c_window.initialize()
 
-        cwindow.hint(cwindow.OPENGL_PROFILE, cwindow.OPENGL_CORE_PROFILE)
-        cwindow.hint(cwindow.OPENGL_FORWARD_COMPAT, True)
-        cwindow.hint(cwindow.CONTEXT_VERSION_MAJOR, 3)
-        cwindow.hint(cwindow.CONTEXT_VERSION_MINOR, 3)
-        cwindow.hint(cwindow.FLOATING, True)
-        cwindow.hint(cwindow.RESIZABLE, False)
+        c_window.hint(c_window.OPENGL_PROFILE, c_window.OPENGL_CORE_PROFILE)
+        c_window.hint(c_window.OPENGL_FORWARD_COMPAT, True)
+        c_window.hint(c_window.CONTEXT_VERSION_MAJOR, 3)
+        c_window.hint(c_window.CONTEXT_VERSION_MINOR, 3)
+        c_window.hint(c_window.FLOATING, True)
+        c_window.hint(c_window.RESIZABLE, False)
         if self.config.samples > 0:
-            cwindow.hint(cwindow.SAMPLES, self.config.samples)
+            c_window.hint(c_window.SAMPLES, self.config.samples)
 
-        self.window = cwindow.create(self.config.width, self.config.height, c_title, NULL, NULL)
-        cwindow.make_context_current(self.window)
-        cwindow.swap_interval(1)
+        self.window = c_window.create(self.config.width, self.config.height, c_title, NULL, NULL)
+        c_window.make_context_current(self.window)
+        c_window.swap_interval(1)
 
     def poll_events(self):
-        cwindow.poll_events()
+        c_window.poll_events()
 
     def swap_buffers(self):
-        cwindow.swap_buffers(self.window)
+        c_window.swap_buffers(self.window)
 
 
 cdef object error_callback
@@ -41,24 +41,24 @@ cdef void on_error(int error_code, const char* description):
     error_callback(description)
 
 cdef object frame_buffer_callback
-cdef void on_frame_buffer_resize(cwindow.Window* window, int width, int height):
+cdef void on_frame_buffer_resize(c_window.Window* window, int width, int height):
     frame_buffer_callback(width, height)
 
 cdef object key_callback
-cdef void on_key_event(cwindow.Window* window, int key, int scancode, int action, int mods):
+cdef void on_key_event(c_window.Window* window, int key, int scancode, int action, int mods):
     key_callback(key, action)
 
 def set_error_callback(Window window, callback):
     global error_callback
     error_callback = callback
-    cwindow.set_error_callback(on_error)
+    c_window.set_error_callback(on_error)
 
 def set_frame_buffer_callback(Window window, callback):
     global frame_buffer_callback
     frame_buffer_callback = callback
-    cwindow.set_resize_callback(window.window, on_frame_buffer_resize)
+    c_window.set_resize_callback(window.window, on_frame_buffer_resize)
 
 def set_key_callback(Window window, callback):
     global key_callback
     key_callback = callback
-    cwindow.set_key_callback(window.window, on_key_event)
+    c_window.set_key_callback(window.window, on_key_event)
--- setup.py
+++ setup.py
@@ -17,78 +17,15 @@ def main():
     distutils.ccompiler.CCompiler.compile = parallel_compile
     use_ccache()
 
-    # TODO clean this up
-    entity_manager = Extension(
-        extra_compile_args=['-std=c++14', '-Wall', '-Werror', '-Wno-unused-function'],
-        extra_link_args=['-std=c++14', '-g'],
-        include_dirs=['src'],
-        language='c++',
-        name='astro.core.entitymanager',
-        sources=['astro/core/entitymanager.pyx', 'src/core/entitymanager.cpp'],
-        undef_macros=['NDEBUG']
-    )
-
-    camera = Extension(
-        extra_compile_args=['-std=c++14', '-Wall', '-Werror', '-Wno-unused-function'],
-        extra_link_args=['-std=c++14', '-g'],
-        include_dirs=['src'],
-        language='c++',
-        name='astro.graphics.camera',
-        sources=['astro/math/matrix.pyx', 'astro/graphics/camera.pyx', 'src/graphics/camera.cpp'],
-        undef_macros=['NDEBUG']
-    )
-
-    vector = Extension(
-        extra_compile_args=['-std=c++14', '-Wall', '-Werror', '-Wno-unused-function'],
-        extra_link_args=['-std=c++14', '-g'],
-        include_dirs=['src'],
-        language='c++',
-        name='astro.math.vector',
-        sources=['astro/math/vector.pyx'],
-        undef_macros=['NDEBUG']
-    )
-
-    matrix = Extension(
-        extra_compile_args=['-std=c++14', '-Wall', '-Werror', '-Wno-unused-function'],
-        extra_link_args=['-std=c++14', '-g'],
-        include_dirs=['src'],
-        language='c++',
-        name='astro.math.matrix',
-        sources=['astro/math/matrix.pyx'],
-        undef_macros=['NDEBUG']
-    )
-
-    renderer = Extension(
-        extra_compile_args=['-std=c++14', '-Wall', '-Werror', '-Wno-unused-function'],
-        extra_link_args=['-std=c++14', '-g'],
-        include_dirs=['src'],
-        language='c++',
-        libraries=['GLEW'],
-        name='astro.graphics.renderer',
-        sources=['astro/graphics/renderer.pyx', 'src/graphics/camera.cpp', 'src/graphics/gl.cpp',
-                 'src/graphics/renderer.cpp', 'src/graphics/model.cpp'],
-        undef_macros=['NDEBUG']
-    )
-
-    shader = Extension(
-        extra_compile_args=['-std=c++14', '-Wall', '-Werror', '-Wno-unused-function'],
-        extra_link_args=['-std=c++14', '-g'],
-        include_dirs=['src'],
-        language='c++',
-        libraries=['GLEW'],
-        name='astro.graphics.shader',
-        sources=['astro/graphics/shader.pyx', 'src/graphics/shader.cpp', 'src/graphics/gl.cpp'],
-        undef_macros=['NDEBUG']
-    )
-
-    window = Extension(
-        extra_compile_args=['-Wall', '-Werror', '-Wno-unused-function'],
-        extra_link_args=['-g'],
-        name='astro.window',
-        libraries=['glfw'],
-        sources=['astro/window.pyx'],
-        undef_macros=['NDEBUG']
-    )
+    module('core.entitymanager', native=['core/entitymanager'])
+    module('graphics.camera', cython=['math/matrix'], native=['graphics/camera'])
+    module('graphics.renderer',
+           native=['graphics/camera', 'graphics/gl', 'graphics/model', 'graphics/renderer'],
+           libraries=['GLEW'])
+    module('graphics.shader', native=['graphics/shader', 'graphics/gl'], libraries=['GLEW'])
+    module('math.vector')
+    module('math.matrix')
+    module('window', cpp=False, libraries=['glfw'])
 
     setup(
         name=PROJECT_NAME,
@@ -96,37 +33,47 @@ def main():
         author_email='byronh@gmail.com',
         version='0.1',
         packages=[PROJECT_NAME],
-        ext_modules=cythonize([entity_manager, camera, vector, matrix, renderer, shader, window])
+        ext_modules=cythonize(_modules)
     )
 
 
-class GameModule(Extension):
-    def __init__(self, name: str, sources: list, libraries: list=None, cpp: bool=True, swig: bool=False,
-                 extra_include_dirs: list=None):
-        extra_compile_args = ['-std=c++14'] if cpp else ['-std=c11']
-        if not isinstance(libraries, list):
-            libraries = []
-        if not isinstance(extra_include_dirs, list):
-            extra_include_dirs = []
-        undef_macros = []
-        if DEBUG:
-            extra_compile_args += ['-Wall', '-Werror']
-            undef_macros += ['NDEBUG']
-        swig_opts = None
-        if swig:
-            name = '_{}'.format(name)
-            swig_opts = ['-c++', '-py3', '-Werror', '-outdir', '{}/native'.format(PROJECT_NAME)]
-        else:
-            name = '{}.native.{}'.format(PROJECT_NAME, name)
-        super().__init__(
-            name=name,
-            sources=sources,
-            include_dirs=['src'] + extra_include_dirs,
-            libraries=libraries,
-            extra_compile_args=extra_compile_args,
-            swig_opts=swig_opts,
-            undef_macros=undef_macros,
-        )
+def module(name: str, cython: list=None, native: list=None, libraries: list=None, cpp: bool=True):
+    name = '{}.{}'.format(PROJECT_NAME, name)
+
+    if cpp:
+        extra_compile_args = ['-std=c++14']
+        extra_link_args = ['-std=c++14']
+        language = 'c++'
+    else:
+        extra_compile_args = ['-std=c11']
+        extra_link_args = ['-std=c11']
+        language = 'c'
+
+    if not isinstance(libraries, list):
+        libraries = []
+
+    undef_macros = []
+    sources = ['{}.pyx'.format(name.replace('.', '/'))]
+    if cython:
+        sources += ['{}/{}.pyx'.format(PROJECT_NAME, path) for path in cython]
+    if native:
+        sources += ['src/{}.{}'.format(path, 'cpp' if cpp else 'c') for path in native]
+    print(name, sources)
+    if DEBUG:
+        extra_compile_args += ['-Wall', '-Werror', '-Wno-unused-function']
+        extra_link_args += ['-g']
+        undef_macros += ['NDEBUG']
+
+    _modules.append(Extension(
+        name=name,
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
+        include_dirs=['src'],
+        language=language,
+        libraries=libraries,
+        sources=sources,
+        undef_macros=undef_macros,
+    ))
 
 
 def parallel_compile(self, sources, output_dir=None, macros=None, include_dirs=None, debug=0, extra_preargs=None,
@@ -154,4 +101,5 @@ def use_ccache():
 
 
 if __name__ == '__main__':
+    _modules = []
     main()
