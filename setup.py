8bb582f1084f40a5b43725746426c586d5046baa
Byron Henze <byronh@gmail.com>
2015-02-22 01:45:43 -0800

    Fix distutils warnings

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from Cython.Build import cythonize
 from multiprocessing import pool
 from setuptools import Extension, setup
 import distutils.ccompiler
-import os
-import subprocess
 
 
 DEBUG = True
@@ -13,41 +11,39 @@ PROJECT_NAME = 'astro'
 def main():
     """ Build the whole engine as a python package """
 
+    module('core.entitymanager', cpp=['core/entitymanager'])
+    module('graphics.camera', cython=['math/matrix'], cpp=['graphics/camera'])
+    module('graphics.renderer',
+           cpp=['graphics/camera', 'graphics/gl', 'graphics/model', 'graphics/renderer'],
+           libraries=['GLEW'])
+    module('graphics.shader', cpp=['graphics/shader', 'graphics/gl'], libraries=['GLEW'])
+    module('math.vector')
+    module('math.matrix')
+    module('window', libraries=['glfw'])
+
+    # Fix annoying warnings
+    fix_distutils()
+
     # Speed up compile times
     distutils.ccompiler.CCompiler.compile = parallel_compile
     use_ccache()
-
-    module('core.entitymanager', native=['core/entitymanager'])
-    module('graphics.camera', cython=['math/matrix'], native=['graphics/camera'])
-    module('graphics.renderer',
-           native=['graphics/camera', 'graphics/gl', 'graphics/model', 'graphics/renderer'],
-           libraries=['GLEW'])
-    module('graphics.shader', native=['graphics/shader', 'graphics/gl'], libraries=['GLEW'])
-    module('math.vector')
-    module('math.matrix')
-    module('window', cpp=False, libraries=['glfw'])
 
     setup(
         name=PROJECT_NAME,
         author='Byron Henze',
         author_email='byronh@gmail.com',
+        url='https://github.com/byronh/astro-engine',
         version='0.1',
         packages=[PROJECT_NAME],
         ext_modules=cythonize(_modules)
     )
 
 
-def module(name: str, cython: list=None, native: list=None, libraries: list=None, cpp: bool=True):
+def module(name: str, cython: list=None, cpp: list=None, libraries: list=None):
     name = '{}.{}'.format(PROJECT_NAME, name)
 
-    if cpp:
-        extra_compile_args = ['-std=c++14']
-        extra_link_args = ['-std=c++14']
-        language = 'c++'
-    else:
-        extra_compile_args = ['-std=c11']
-        extra_link_args = ['-std=c11']
-        language = 'c'
+    extra_compile_args = ['-std=c++14']
+    extra_link_args = ['-std=c++14']
 
     if not isinstance(libraries, list):
         libraries = []
@@ -56,8 +52,8 @@ def module(name: str, cython: list=None, native: list=None, libraries: list=None
     sources = ['{}.pyx'.format(name.replace('.', '/'))]
     if cython:
         sources += ['{}/{}.pyx'.format(PROJECT_NAME, path) for path in cython]
-    if native:
-        sources += ['src/{}.{}'.format(path, 'cpp' if cpp else 'c') for path in native]
+    if cpp:
+        sources += ['src/{}.cpp'.format(path) for path in cpp]
     print(name, sources)
     if DEBUG:
         extra_compile_args += ['-Wall', '-Werror', '-Wno-unused-function']
@@ -69,11 +65,20 @@ def module(name: str, cython: list=None, native: list=None, libraries: list=None
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
         include_dirs=['src'],
-        language=language,
+        language='c++',
         libraries=libraries,
         sources=sources,
         undef_macros=undef_macros,
     ))
+
+
+def fix_distutils():
+    """ Fix the annoying warnings caused by distutils bug """
+    import distutils.sysconfig
+
+    cfg_vars = distutils.sysconfig.get_config_vars()
+    if 'CFLAGS' in cfg_vars:
+        cfg_vars['CFLAGS'] = cfg_vars['CFLAGS'].replace('-Wstrict-prototypes', '')
 
 
 def parallel_compile(self, sources, output_dir=None, macros=None, include_dirs=None, debug=0, extra_preargs=None,
@@ -95,6 +100,9 @@ def parallel_compile(self, sources, output_dir=None, macros=None, include_dirs=N
 
 def use_ccache():
     """ Use ccache to speed up c++ compile times, if available """
+    import os
+    import subprocess
+
     result = subprocess.call(['which', 'ccache'])
     if result == 0:
         os.environ['CC'] = 'ccache gcc'
